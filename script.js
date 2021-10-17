f8ae257ed8cb3f37f34f307f011cee4cd2a01eb8
Ram Linux <ram97-boop@users.noreply.github.com>
2021-10-18 01:43:26 +0200

    Add minor changes to how the menu button disappears behind the sliding menu

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function showMenu(){
 	document.getElementById("navLinks").style.left = "0";
+//	document.getElementById("showMenuButton").style.display = "none";
 }
 
 function hideMenu(){
 	document.getElementById("navLinks").style.left = "-210px";
+//	document.getElementById("showMenuButton").style.display = "block";
 }
